Corrected errors with FID metric calculation

train.py
@@ -26,8 +26,8 @@ import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
 
 # Mixed Pricision Training
-from torch.amp import autocast, GradScaler
-# from torch.cuda.amp import GradScaler
+from torch.amp import autocast
+from torch.cuda.amp import GradScaler
 
 from models.VAE import TransformerVAE
 from data_loader import RobotDataset
@@ -36,7 +36,7 @@ from skimage.metrics import peak_signal_noise_ratio, normalized_root_mse, struct
 from torch_fidelity import calculate_metrics
 
 from datetime import datetime
-from utils import set_seed, load_data, set_distributed_training, initialize_model_ddp, plot_history, save_examples, frechet_distance, get_beta_cyclical
+from utils import set_seed, load_data, set_distributed_training, initialize_model_ddp, plot_history, save_examples, frechet_distance, get_beta_cyclical, inception_preprocess
 from tqdm import tqdm
 
 from torchvision.models import inception_v3
@@ -228,7 +228,7 @@ def eval(model, model_config, val_dataloader, ddp_config):
     # Load best checkpoint
     if local_rank == 0:
         best_ckpt = torch.load(os.path.join(ckpt_dir, 'best_ckpt.pth'))
-        model.module.load_state_dict(best_ckpt['model_state_dict'])
+        model.module.load_state_dict(best_ckpt['model_state_dict'], strict=False)
         
     # print(torch.cuda.memory_summary(device))
     model.eval()
@@ -270,8 +270,10 @@ def eval(model, model_config, val_dataloader, ddp_config):
             ##################
             
             # Collect FID features
-            real_feat = inception(batch).detach()
-            fake_feat = inception(recon_batch).detach()
+            batch_preprocessed = inception_preprocess(batch)
+            recon_preprocessed = inception_preprocess(recon_batch)
+            real_feat = inception(batch_preprocessed).detach()
+            fake_feat = inception(recon_preprocessed).detach()
             
             # real_features.append(real_feat)
             # fake_features.append(fake_feat)
@@ -284,7 +286,7 @@ def eval(model, model_config, val_dataloader, ddp_config):
             for o, r in zip(batch_np, recon_np):
                 psnr_list.append(peak_signal_noise_ratio(o, r, data_range=1.0))
                 nrmse_list.append(normalized_root_mse(o, r))
-                ssim_list.append(structural_similarity(o, r, multichannel=True, data_range=1.0, channel_axis=0)) # (C, H, W)
+                ssim_list.append(structural_similarity(o, r, multichannel=False, data_range=1.0, channel_axis=0)) # (C, H, W)
                 
             # Sum up the metrics from all GPUs     
             total_psnr += torch.tensor(psnr_list, device=device).sum()
@@ -366,8 +368,6 @@ def eval(model, model_config, val_dataloader, ddp_config):
     dist.barrier() 
     # End distributed processing
     dist.destroy_process_group()
-    
-    return metrics
     
 def get_config(args):
     

utils.py
@@ -9,6 +9,8 @@ from models.VAE import TransformerVAE
 import os
 from scipy.linalg import sqrtm
 from matplotlib import pyplot as plt
+
+import torch._dynamo as dynamo
 # Distributed training
 import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
@@ -159,8 +161,7 @@ def initialize_model_ddp(model_config, ddp_config):
     model = TransformerVAE(img_size=image_size, patch_size=16, latent_dim=128, in_channels=in_channels).to(device)
     optimizer = optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
     model = DDP(model, device_ids=[ddp_config["local_rank"]], output_device=ddp_config["local_rank"]) # DDP Wrapping
-    model = torch.compile(model) # Torch Compile Wrapping
-    
+    model = torch.compile(model, backend='eager') # Torch Compile Wrapping
     scaler = GradScaler() # Automatic Mixed Precision Scaler
     
     return model, optimizer, scaler
@@ -193,10 +194,10 @@ def save_examples(orig_batch, recon_batch, save_dir):
     
     fig, axes = plt.subplots(1, 2, figsize=(10, 5), constrained_layout=True)
     
-    axes[0].imshow(orig)
+    axes[0].imshow(orig, cmap='gray')
     axes[0].set_title('Original Image')
     axes[0].axis('off')
-    axes[1].imshow(recon)
+    axes[1].imshow(recon, cmap='gray')
     axes[1].set_title('Reconstructed Image')
     axes[1].axis('off')
     
@@ -241,3 +242,21 @@ def get_beta_cyclical(iteration, cycle_length, min_beta, max_beta):
     beta = min_beta + (max_beta - min_beta) * np.maximum(0, (1 - x))
     
     return beta
+
+def inception_preprocess(image_batch):
+    """
+    Preprocess images for Inception model.
+    Args:
+        images: Images to preprocess. (Batch, C, H, W)
+    Returns:
+        preprocessed_images: Preprocessed images. (Batch, C, H, W)
+    """
+    
+    # Resize and normalize the images
+    
+    preprocess = v2.Compose([v2.Resize((299,299)),
+                             v2.Grayscale(num_output_channels=3),
+                             v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]) # Use ImageNet mean and std so the activations land in the same range as Inception-v3 was trained on
+    
+    
+    return preprocess(image_batch)